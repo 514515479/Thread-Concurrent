547971d97b8d9191d08454047b5cdac62de29c5f
tobi <514515479@qq.com>
2020-06-28 22:35:25 +0800

    JDK自带线程池;

--- Thread-ConCurrent/src/threadPool/TestThreadPoolExecutor.java
+++ Thread-ConCurrent/src/threadPool/TestThreadPoolExecutor.java
@@ -1,8 +1,9 @@
 package threadPool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -12,10 +13,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  * ThreadPoolExecutor
  **/
 public class TestThreadPoolExecutor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         method1();
         method2();
-
+        method3();
+        submit();
+        invokeAll();
     }
 
     /**
@@ -79,5 +82,83 @@ public class TestThreadPoolExecutor {
             System.out.println(Thread.currentThread().getName() + ":3");
         });
         pool.shutdown();
+    }
+
+    /**
+     * 单线程线程池newSingleThreadExecutor
+     * 特点：
+     *     1.多个任务排队执行。线程数固定为1，任务数多于1时，会放入无界队列排队。任务执行完毕，这唯一的线程也不会被释放。
+     *     2.自己创建一个单线程串行执行任务，如果任务执行失败而终止那么没有任何补救措施，而线程池还会新建一个线程，保证池的正常工作.
+     *
+     * 适用：
+     *     整个线程池表现为线程数会根据任务量不断增长，没有上限，当任务执行完毕，空闲1分钟后释放线程。
+     *     适合任务数比较密集，但每个任务执行时间较短的情况
+     **/
+    public static void method3() {
+        //ExecutorService pool = Executors.newSingleThreadExecutor();
+        ExecutorService pool = Executors.newSingleThreadExecutor(new ThreadFactory() {
+            private AtomicInteger t = new AtomicInteger(1);
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "线程" + t.getAndIncrement());
+            }
+        });
+        pool.execute(() -> {
+            System.out.println(Thread.currentThread().getName() + ":1");
+        });
+        pool.execute(() -> {
+            System.out.println(Thread.currentThread().getName() + ":2");
+        });
+        pool.execute(() -> {
+            System.out.println(Thread.currentThread().getName() + ":3");
+        });
+        pool.shutdown();
+    }
+
+    //submit方法
+    public static void submit() {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        Future<String> future = pool.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("running...");
+                Thread.sleep(1000);
+                return "ok";
+            }
+        });
+        try {
+            System.out.println(future.get());
+            pool.shutdown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //invokeAll方法
+    public static void invokeAll() throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        List<Future<Object>> list = pool.invokeAll(Arrays.asList(
+                () -> {
+                    System.out.println("begin");
+                    Thread.sleep(1000);
+                    return "1";
+                },
+                () -> {
+                    System.out.println("begin");
+                    Thread.sleep(1000);
+                    return "2";
+                }
+        ));
+        list.forEach( x -> {
+            try {
+                System.out.println(x.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
